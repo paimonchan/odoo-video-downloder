160fee4e71865f87b35dece5a2e39bafc0da624c
Paimon <n.h.shounen@gmail.com>
2022-04-14 09:10:23 +0700

    [NA] add manifest

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Youtube Downloader",
+    'summary': """Odoo Youtube Downloader""",
+    'description': """Youtube Downloader""",
+    'author': "Paimon",
+    'category': 'other',
+    'version': '1.0.1',
+    'depends': ['base'],
+    'data': [
+        'security/youtube_downloader.xml',
+    ],
+}
