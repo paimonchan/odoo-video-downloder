f09155a5e8731ded992cd7b9d5f5991f8116509c
Paimon <n.h.shounen@gmail.com>
2022-04-20 23:19:37 +0700

    [NA] rename name module

--- __manifest__.py
+++ __manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Odoo Youtube Downloader",
-    'summary': """Odoo Youtube Downloader""",
-    'description': """Youtube Downloader""",
+    'name': "Odoo Video Downloader",
+    'summary': """Odoo Video Downloader""",
+    'description': """Video Downloader""",
     'author': "Paimon",
     'category': 'other',
     'version': '1.0.1',
